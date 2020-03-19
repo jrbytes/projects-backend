be770c2b7fae3509ce5a4fdbc0b2d9a67bc1dd8f
JrBytes <itamazor@gmail.com>
2020-03-18 21:23:08 -0300

    Implementado variável da porta e retornado no método listen

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
+const port = 3333
 
 require('./database')
 
@@ -10,6 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
-  console.log('LiveLongAndProsper...')
+app.listen(port, () => {
+  console.log(`LiveLongAndProsper... port ${port}`)
 })
